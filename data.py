cbca793da0832c0c593bcf4aeee839e2b6463b88
Ali Atallah Faris Al Sariera <sariera@live.com>
2018-12-08 19:32:47 +0200

    Creating popular_article function to print the most popular three articles of all time

--- data.py
+++ data.py
@@ -12,3 +12,24 @@ def connect(dbname='news'):
         return (db, c)
     except:
         print 'An error occurred while connecting to database'
+
+
+def popular_article():
+    '''print the most popular three articles of all time'''
+
+    (db, c) = connect()
+    query = \
+        """select title, count(title) as views from log, articles
+    where log.path = concat('/article/', articles.slug)
+    group by title
+    order by views desc
+    limit 3
+    """
+    c.execute(query)
+    result = c.fetchall()
+    db.clode()
+    print '''
+Popular Articles:
+'''
+    for i in range(0, len(result), 1):
+        print '"' + result[i][0] + '" - ' + str(result[i][1]) + ' views'
