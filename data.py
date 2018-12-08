fd3e08d0b3619872081eed3fdabc3d82f6ac60a5
Ali Atallah Faris Al Sariera <sariera@live.com>
2018-12-08 19:29:41 +0200

    Creating connect function to connect to POSTGRESQL database

--- data.py
+++ data.py
@@ -1,3 +1,14 @@
 #!/usr/bin/python
 # -*- coding: utf-8 -*-
 import psycopg2
+
+
+def connect(dbname='news'):
+    '''Connecting to POSTGRESQL database'''
+
+    try:
+        db = psycopg2.connect('dbname = news')
+        c = db.cursor()
+        return (db, c)
+    except:
+        print 'An error occurred while connecting to database'
